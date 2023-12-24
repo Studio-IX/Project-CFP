e2e2c58671b332728f7018ac1dee74729d91c6e8
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2023-12-24 19:16:35 +0000

    update with vercel configs

--- Project_CFP/asgi.py
+++ Project_CFP/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Project_CPP project.
+ASGI config for Project_CFP project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project_CPP.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project_CFP.settings')
 
 application = get_asgi_application()
--- Project_CFP/urls.py
+++ Project_CFP/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Project_CPP project.
+URL configuration for Project_CFP project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
