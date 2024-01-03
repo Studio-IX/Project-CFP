12fe3ed1ce0c50c028bceaa6df9fcddf0f7c75cf
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2024-01-03 22:29:12 +0000

    completed the signup process

--- Project_CFP/urls.py
+++ Project_CFP/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include("accounts.urls"))
+    path('', include("accounts.urls"))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
