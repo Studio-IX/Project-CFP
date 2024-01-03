cad3551864c61fffcd370754a5f2af8422d5ead1
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2024-01-03 22:27:56 +0000

    completed the signup process

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('', register01 , name="register01"),
+    path('register02/', register02 , name="register02"),
+    path('register03/', register03 , name="register03"),
+    path('register04/', register04 , name="register04"),
+    path('register05/', register05 , name="register05"),
+]
---
+++ accounts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def register01(request):
+    return render(request, "accounts/signup/01.html")
+
+def register02(request):
+    return render(request, "accounts/signup/02.html")
+
+def register03(request):
+    return render(request, "accounts/signup/03.html")
+
+def register04(request):
+    return render(request, "accounts/signup/04.html")
+
+def register05(request):
+    return render(request, "accounts/signup/05.html")
